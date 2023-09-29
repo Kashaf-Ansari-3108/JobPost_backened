0cff7cba3f4e7c06d38c389ce65c295298adfa3b
Kashaf-Ansari-3108 <kashafansari3108@gmail.com>
2023-09-29 08:58:07 +0500

    cors

--- app.js
+++ app.js
@@ -2,33 +2,32 @@ const express = require('express');
 const userRouter = require('./routes/UserRoutes');
 const jobRouter = require('./routes/JobRoutes');
 const connectDB = require('./db/db');
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config({path:'./config.env'});
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
-// Custom CORS middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
+app.get("/sample", (req,res)=>{
+    res.send("API HIT!!!");
 });
 
-app.get("/sample", (req, res) => {
-  res.send("API HIT!!!");
-});
 
-// Connect to the database
+//connectDB
 connectDB();
 
-// Define routes and handle JSON requests
-app.use(express.json());
-app.use('/api/user', userRouter);
-app.use('/api/job', jobRouter);
+//middleware
 
-// Start the server
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+//routes
+app.use(express.json());
+
+//connect app
+app.use('/api/user',userRouter);
+app.use('/api/job',jobRouter);
+
+
+
+
+
+app.listen(PORT, ()=>console.log(`Server is running on PORTTT ${PORT}`));
